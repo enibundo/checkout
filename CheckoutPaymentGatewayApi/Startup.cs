aeb4d5d46908241bf0f984f07eefffc316c91aec
Eni BUNDO <ebundo@protegys.com>
2021-03-21 08:20:28 +0100

    Add swagger

--- CheckoutPaymentGatewayApi/Startup.cs
+++ CheckoutPaymentGatewayApi/Startup.cs
@@ -32,11 +32,22 @@ namespace CheckoutPaymentGatewayApi
             services.AddSingleton<IPaymentRepository, MemoryPaymentRepository>();
 
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger(); 
+            app.UseSwagger(c =>
+            {
+                c.SerializeAsV2 = true;
+            });
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             app.UseMiddleware<CustomErrorHandlerMiddleware>();
 
             if (env.IsDevelopment())
